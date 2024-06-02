be2336858365eb0e9fadb65ab51858e885f5e170
ehuyn <163863983+ehuyn@users.noreply.github.com>
2024-06-02 12:21:28 -0400

    - Coded input collection from JTextFields and JPasswordFields - Created function that checks input from register screen fields     - returns boolean (true for valid input, false for invalid) - Added button that take users from register screen to login screen and a login button that will take users from login screen to menu screen.

--- src/OpeningScreen.java
+++ src/OpeningScreen.java
@@ -10,12 +10,16 @@ public class OpeningScreen extends JFrame implements ActionListener{
     private JPanel registerScreen;
     private JButton loginButton;
     private JButton registerButton;
-    private JButton registerButton2;
-    private JTextField textField1;
-    private JPasswordField passwordField1;
-    private JPasswordField passwordField2;
-    private JPasswordField passwordField3;
+    private JButton toRegisterButton;
+    private JTextField createUsernameField;
+    private JPasswordField passwordField;
+    private JPasswordField createPasswordField;
+    private JPasswordField confirmPasswordField;
     private JButton createAccountButton;
+    private JButton loginToMenuButton;
+    private JTextField usernameField;
+    private JTextField nameField;
+    private JButton toLoginButton;
 
     public OpeningScreen(){
         intialize();
@@ -41,22 +45,57 @@ public class OpeningScreen extends JFrame implements ActionListener{
     }
 
     private void loginScreen(){
-        registerButton2.addActionListener(this);
+        toRegisterButton.addActionListener(this);
+        loginToMenuButton.addActionListener(this);
     }
 
     private void registerScreen(){
+        createAccountButton.addActionListener(this);
+        toLoginButton.addActionListener(this);
+    }
+
+    private boolean checkRegisterInfo(String name, String username, String password, String confirmPassword){
+        if(!name.matches("[a-zA-Z]+")){
+            System.out.println("Name can only consist of alphabetical characters.");
+            return false;
+        }
+        else if(!username.matches("[a-zA-Z]+")){
+            System.out.println("Username can only consist of alphabetical characters.");
+            return false;
+        }
+        else if(password.length() > 12 || password.length() < 6){
+            System.out.println("Password must be from 6 to 12 characters long.");
+            return false;
+        }
+        else if(!password.equals(confirmPassword)){
+            System.out.println("Password and confirm password entries do not match.");
+            return false;
+        }
+        return true;
     }
 
     @Override
     public void actionPerformed(ActionEvent e){
-        if(e.getSource() == loginButton){
+        if(e.getSource() == loginButton || e.getSource() == toLoginButton){
             openingScreen.setVisible(false);
+            registerScreen.setVisible(false);
             loginScreen.setVisible(true);
         }
-        else if(e.getSource() == registerButton || e.getSource() == registerButton2){
+        else if(e.getSource() == registerButton || e.getSource() == toRegisterButton){
             openingScreen.setVisible(false);
             loginScreen.setVisible(false);
             registerScreen.setVisible(true);
+        }
+        else if(e.getSource() == loginToMenuButton){
+            String username = usernameField.getText();
+            String password = passwordField.getText();
+        }
+        else if(e.getSource() == createAccountButton){
+            String name = nameField.getText();
+            String username = createUsernameField.getText();
+            String password = createPasswordField.getText();
+            String confirmPassword = confirmPasswordField.getText();
+            boolean valid = checkRegisterInfo(name, username, password, confirmPassword);
         }
     }
 }
